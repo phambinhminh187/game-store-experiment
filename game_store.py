6aa4d3ae595a003ab4b392580476b4449e52257e
Pham Binh Minh <binhminh.pham187@gmail.com>
2026-09-19 17:07:45 +0700

    Update game_store.py

--- game_store.py
+++ game_store.py
@@ -1,5 +1,5 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
-from PySide6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6 import uic
 import sys, webbrowser, webview
 
 def launcher(window_title, target):
